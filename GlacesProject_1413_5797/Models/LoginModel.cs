10b9edadca4e8adba8122f9675c149208896fe67
Raphael Bloch <rbloch@g.jct.ac.il>
2020-03-19 17:34:46 +0200

    mes ajouts d hier

---
+++ GlacesProject_1413_5797/Models/LoginModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using BL;
+
+namespace PL.Models
+{
+    public class LoginModel
+    {
+        public Shop CurrentShop { get; set; }
+
+        public Bl MyBl { get; set; }
+
+
+        public bool findShopByLogin(string id, string password)
+        {
+            Shop shop = MyBl.findShopByLogin(id, password);
+
+            if (shop == null)
+                return false;
+            else
+            {
+                CurrentShop = shop;
+                return true;
+            }
+
+
+        }
+    }
+}
---
+++ GlacesProject_1413_5797/ViewModel/LoginVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PL.Models;
+using PL.Views;
+using System.ComponentModel;
+
+namespace PL.ViewModel
+{
+    public class LoginVM : INotifyPropertyChanged
+    {
+        public LoginVM(LoginUC LoginUC)
+        {
+            CurrentModel = new LoginModel();
+            this.LoginUC = LoginUC;
+        }
+
+        public LoginModel CurrentModel { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private LoginUC LoginUC;
+
+    }
+}
