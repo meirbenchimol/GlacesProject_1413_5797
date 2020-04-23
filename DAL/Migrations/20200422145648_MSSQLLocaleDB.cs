1eff7b23847dceed5fd962fabab675d362a30a01
meir <benchimolmeir@gmail.com>
2020-04-23 21:49:28 +0300

    meir

---
+++ DAL/Migrations/20200422145648_MSSQLLocaleDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class MSSQLLocaleDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IceCreams_Shops_ShopId",
+                table: "IceCreams");
+
+            migrationBuilder.DropIndex(
+                name: "IX_IceCreams_ShopId",
+                table: "IceCreams");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_IceCreams_ShopId",
+                table: "IceCreams",
+                column: "ShopId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IceCreams_Shops_ShopId",
+                table: "IceCreams",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
